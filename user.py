ece765681853ff62af98dbb712d1a9a83057c363
naf <muirurigathairu@gmail.com>
2020-06-30 11:00:25 -0400

    creates User class with initialization function

---
+++ user.py
@@ -0,0 +1,11 @@
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+    
+  def pro(self):
+    print("welcome to Allsafe\n\nan application that securely stores your various online \naccounts details and their respective passwords.")
+
+p1 = Person("John", 36)
+
+p1.pro()
