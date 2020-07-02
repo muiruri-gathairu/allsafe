a6aca040358065cc8bb9ee57e4f366482a82ffa6
naf <muirurigathairu@gmail.com>
2020-07-02 14:37:54 -0400

    updates details class

--- details.py
+++ details.py
@@ -1,2 +1,16 @@
 from user import Users
 
+class Details:
+
+        bb = []  # Empty credentials list
+
+        @classmethod
+        def check_user(cls, username, password):
+                current_user = ''
+                # current_password= ''
+                for user in Users.aa:
+                      if (user.username == username and user.password == password):
+                          current_user = user.username
+                        #   current_password = user.password
+                          return current_user
+                        #  return current_password
