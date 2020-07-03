f94bc12153111fb20f72d25f26538192e26f0b5d
naf <muirurigathairu@gmail.com>
2020-07-02 23:54:09 -0400

    adds save details test function

--- details_test.py
+++ details_test.py
@@ -1,5 +1,5 @@
-import unittest 
 from details import Details
+import unittest 
 
 class TestDetails(unittest.TestCase):
     
@@ -12,3 +12,8 @@ class TestDetails(unittest.TestCase):
         self.assertEqual(self.bb.username,"Naf")
         self.assertEqual(self.bb.password,"12345678")
 
+    def test_save_details(self):
+
+        self.bb.save_details() 
+        self.assertEqual(len(Details.details_list),1)
+        
